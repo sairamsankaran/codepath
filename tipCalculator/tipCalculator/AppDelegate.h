18934dd5316bcf538b03591088c0463e628e9b8e
Sairam Sankaran <sairam.sankaran@gmail.com>
2013-09-17 22:22:08 -0700

    first commit

---
+++ tipCalculator/tipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Sairam Sankaran on 9/17/13.
+//  Copyright (c) 2013 Sairam Sankaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipCalculator/tipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipCalculator
+//
+//  Created by Sairam Sankaran on 9/17/13.
+//  Copyright (c) 2013 Sairam Sankaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
